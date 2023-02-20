4f813fd8db93f8783030ea1aead9fcab7da1ec16
John Poston <john.poston2@outlook.com>
2023-02-20 17:51:53 -0500

    initialized ToDoList

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './interfaces/Todo';
 
 @Component({
   selector: 'app-to-do',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class ToDoComponent {
 
-}
+  ToDoList : Todo[] = [
+      {task:"Finish project!", completed:false},
+      {task:"Go grocery shopping", completed:true},
+      {task:"Do laundry", completed:false},
+      {task:"Make dinner", completed:true},
+      {task:"Walk the dogs", completed:true},
+      {task:"Vacuum the house", completed:false},
+      {task:"Fix broken thing", completed:false},
+    ];
+  }
